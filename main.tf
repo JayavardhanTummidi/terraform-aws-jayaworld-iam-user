94d1abe594d0c5147ea2e37457c9a31e5998c8ff
Jayavardhan Tummidi <jayavardhantummidi@Jayavardhans-MacBook-Pro.local>
2021-09-20 20:58:00 -0400

    iam-user

--- main.tf
+++ main.tf
@@ -1,46 +1,46 @@
 provider "aws" {
-    region = var.region
+  region = var.region
 }
 
 # For IAM user creation 
 resource "aws_iam_user" "jayaworld-iam-user" {
-    name = var.iam_user_name
-    path = "/"
-    permissions_boundary = var.iam_user_boundary_policy_arn
-    force_destroy = var.force_destroy
-    tags = merge(var.tags)
+  name                 = var.iam_user_name
+  path                 = "/"
+  permissions_boundary = var.iam_user_boundary_policy_arn
+  force_destroy        = var.force_destroy
+  tags                 = merge(var.tags)
 }
 
 # For adding users to the groups - create group membership resources
 resource "aws_iam_user_group_membership" "jayaworld-iam-group-membership" {
-    user = aws_iam_user.jayaworld-iam-user.name
-    groups = var.iam_groups
+  user   = aws_iam_user.jayaworld-iam-user.name
+  groups = var.iam_groups
 }
 
 # Manages an IAM User Login Profile with limited support for password creation during Terraform resource creation. Uses PGP to encrypt the password for safe transport to the user. PGP keys can be obtained from Keybase.
 resource "aws_iam_user_login_profile" "jayaworld-iam-login-profile" {
-    user = aws_iam_user.jayaworld-iam-user.name
-    pgp_key = var.pgp_key
-    password_length = var.iamuser_password_length
-    password_reset_required = var.iamuser_password_reset_required
+  user                    = aws_iam_user.jayaworld-iam-user.name
+  pgp_key                 = var.pgp_key
+  password_length         = var.iamuser_password_length
+  password_reset_required = var.iamuser_password_reset_required
 }
 
 # Provides an IAM access key. This is a set of credentials that allow API requests to be made as an IAM user
 resource "aws_iam_access_key" "jayaworld-iam-access-key" {
-    user = aws_iam_user.jayaworld-iam-user.name
-    pgp_key = var.pgp_key
-    status = var.iam_access_key_status
+  user    = aws_iam_user.jayaworld-iam-user.name
+  pgp_key = var.pgp_key
+  status  = var.iam_access_key_status
 }
 
 # Provides an IAM policy attached to a user.
 resource "aws_iam_user_policy" "jayaworld-iam-user-policy" {
-    user = aws_iam_user.jayaworld-iam-user.name
-    name = var.user_inline_policy_name
-    policy = var.user_inline_policy   
+  user   = aws_iam_user.jayaworld-iam-user.name
+  name   = var.user_inline_policy_name
+  policy = var.user_inline_policy
 }
 
 # Attaches a Managed IAM Policy to an IAM user
 resource "aws_iam_user_policy_attachment" "jayaworld-iam-user-managed-policy" {
-    user = aws_iam_user.jayaworld-iam-user.name
-    policy_arn = var.user_managed_policy_arn
+  user       = aws_iam_user.jayaworld-iam-user.name
+  policy_arn = var.user_managed_policy_arn
 } 
--- variables.tf
+++ variables.tf
@@ -1,76 +1,95 @@
 variable "region" {
-    description = "specify region"
-    type = string
-    default = "us-east-1"
+  description = "specify region"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "iam_user_name" {
-    description = "The user's name. The name must consist of upper and lowercase alphanumeric characters with no spaces. You can also include any of the following characters: =,.@-_.. User names are not distinguished by case. For example, you cannot create users named both TESTUSER and testuser"
-    type = string
+  description = "The user's name. The name must consist of upper and lowercase alphanumeric characters with no spaces. You can also include any of the following characters: =,.@-_.. User names are not distinguished by case. For example, you cannot create users named both TESTUSER and testuser"
+  type        = string
 }
 
 variable "iam_user_boundary_policy_arn" {
-    description = "The ARN of the policy that is used to set the permissions boundary for the user"
-    type = string
-    default = ""
+  description = "The ARN of the policy that is used to set the permissions boundary for the user"
+  type        = string
+  default     = ""
 }
 
 variable "force_destroy" {
-    description = "When destroying this user, destroy even if it has non-Terraform-managed IAM access keys, login profile or MFA devices. Without force_destroy a user with non-Terraform-managed access keys and login profile will fail to be destroyed."
-    type = string
-    default = false
+  description = "When destroying this user, destroy even if it has non-Terraform-managed IAM access keys, login profile or MFA devices. Without force_destroy a user with non-Terraform-managed access keys and login profile will fail to be destroyed."
+  type        = string
+  default     = false
 }
 
 variable "tags" {
-    description = "Tags for the user"
-    type = map(string)
-    default = {}
+  description = "Tags for the user"
+  type        = map(string)
+  default     = {}
 }
 
 variable "iam_groups" {
-    description = "Add users to the groups"
-    type = list(string)
-    default = []
+  description = "Add users to the groups"
+  type        = list(string)
+  default     = []
 }
 
 variable "pgp_key" {
-    description = "Either a base-64 encoded PGP public key, or a keybase username in the form keybase:username. Only applies on resource creation. Drift detection is not possible with this argument."
-    type = string
-    default = ""
+  description = "Either a base-64 encoded PGP public key, or a keybase username in the form keybase:username. Only applies on resource creation. Drift detection is not possible with this argument."
+  type        = string
+  default     = ""
 }
 
 variable "iamuser_password_length" {
-    description = "The length of the generated password on resource creation. Only applies on resource creation. Drift detection is not possible with this argument."
-    type = number
-    default = 20
+  description = "The length of the generated password on resource creation. Only applies on resource creation. Drift detection is not possible with this argument."
+  type        = number
+  default     = 20
 }
 
 variable "iamuser_password_reset_required" {
-    description = "Whether the user should be forced to reset the generated password on resource creation. Only applies on resource creation. Drift detection is not possible with this argument."
-    type = bool
-    default = true
+  description = "Whether the user should be forced to reset the generated password on resource creation. Only applies on resource creation. Drift detection is not possible with this argument."
+  type        = bool
+  default     = true
 }
 
 variable "iam_access_key_status" {
-    description = "Access key status to apply. Defaults to Active. Valid values are Active and Inactive"
-    type = string
-    default = "Active"
+  description = "Access key status to apply. Defaults to Active. Valid values are Active and Inactive"
+  type        = string
+  default     = "Active"
 }
 
 variable "user_inline_policy_name" {
-    description = "The name of the policy. If omitted, Terraform will assign a random, unique name"
-    type = string
-    default = ""
+  description = "The name of the policy. If omitted, Terraform will assign a random, unique name"
+  type        = string
+  default     = ""
 }
 
 variable "user_inline_policy" {
-    description = "The policy document. This is a JSON formatted string."
-    type = string
-    default = null
+  description = "The policy document. This is a JSON formatted string."
+  type        = string
+  default     = " "
 }
 
 variable "user_managed_policy_arn" {
-    description = "The ARN of the policy you want to apply"
-    type = string
-    default = ""
+  description = "The ARN of the policy you want to apply"
+  type        = string
+  default     = <<EOF
+    {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "iam:GenerateCredentialReport",
+                "iam:GenerateServiceLastAccessedDetails",
+                "iam:Get*",
+                "iam:List*",
+                "iam:SimulateCustomPolicy",
+                "iam:SimulatePrincipalPolicy"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+
 }
